649e20125a7a0614767dc0783502e55a76431358
User@CNSDECELI10003T <enix@antrui.com>
2013-08-30 15:24:49 +0800

    draw_from_dat.py: draw data from binary file and insert into db

--- utils/db_import.py
+++ utils/db_import.py
@@ -3,9 +3,14 @@ from mysql import Mysql
 class db(object):
 
     def __init__(self):
-        self.db = Mysql(host='172.29.138.91', db='stock', user='root', password='enixyuabc@123')
+        self.db = Mysql(host='172.29.138.91', db='stock', user='istock', password='enixyuabc')
         self.db.connect()
 
+
+    def insertSQL(self, table, columnValueDict):
+        sql = 'INSERT INTO %s(%s) VALUES(%s)' % (table, ",".join(columnValueDict.keys()), ",".join(self.to_str(columnValueDict.values())))
+        return self.db.execute(sql)
+    
     def insert(self, table, columns, data):        
         sql = 'INSERT INTO %s(%s) VALUES(%s)' % (table, ",".join(columns), ",".join(self.to_str(data)))
         return self.db.execute(sql)
